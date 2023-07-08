fb63e0a282195def8422c986cfb89d5527d25c9b
Octávio Correia <octavio.correia2017@gmail.com>
2023-07-08 18:16:10 -0300

    Esqueleto
    Esqueleto principal do site montado. Falta deixar bonito

---
+++ script.js
@@ -0,0 +1,22 @@
+const imgs = document.getElementById("img")
+const img = document.querySelectorAll("#img img")
+
+var idx = 0
+
+function frente() {
+    idx++
+    if (idx > img.length - 1) {
+        idx = 0
+    }
+
+    imgs.style.transform = `translateX(${-idx * 300}px)`
+}
+
+function tras() {
+    idx--
+    if (idx < 0) {
+        idx = img.length - 1
+    }
+
+    imgs.style.transform = `translateX(${-idx * 300}px)`
+}
