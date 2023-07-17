f45f473622ac7831a95a3f17c1fa7b4d0f62073d
Octávio Correia <octavio.correia2017@gmail.com>
2023-07-17 16:34:25 -0300

    Atualizações no código

--- scriptOrcamento.js
+++ scriptOrcamento.js
@@ -4,3 +4,8 @@ const profissao = document.querySelector("#inputProfissao")
 const contato = document.querySelector("#inputTelefone")
 const email = document.querySelector("#inputEmail")
 const nave = document.querySelector("#selectNave")
+
+
+function show(){
+    alert(`Olá ${nome.value}, ${profissao.value}. A nave selcionada foi ${nave.value}. Seja feliz com ela. Entrarei em contato no número ${contato.value} ou através do ${email.value}`)
+}
